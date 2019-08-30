4c63d6263f7428fb59f69f55ae0262d8afa09e96
RobertoTheWizardo <oscar@werbig.com>
2019-08-30 18:17:06 +0200

    removed none working code
    removed test task

--- Source/Thief/TETestTask.cpp
+++ Source/Thief/TETestTask.cpp
@@ -1,5 +1,5 @@
 // Fill out your copyright notice in the Description page of Project Settings.
-
+/*
 #include "TETestTask.h"
 
 EBTNodeResult::Type UTETestTask::ExecuteTask(UBehaviorTreeComponent & OwnerComp, uint8 * NodeMemory)
@@ -7,3 +7,4 @@ EBTNodeResult::Type UTETestTask::ExecuteTask(UBehaviorTreeComponent & OwnerComp,
 	UE_LOG(LogTemp, Warning, TEXT("whoa it worked"));
 	OnTaskFinished();
 }
+*/
